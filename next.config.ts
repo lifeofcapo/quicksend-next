ac0625eb0a44b94cebbde018b42626aaf20e1317
lifeofcapo <timecuresboy@gmail.com>
2025-11-02 16:53:58 +0300

    fdsofds

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    webpackBuildWorker: true,
+  },
   output: "standalone",
   
   webpack: (config, { isServer }) => {
