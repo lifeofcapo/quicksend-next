24787e7aa0eb710b2071cfb12aa3a077fc5a308b
lifeofcapo <timecuresboy@gmail.com>
2025-11-12 12:38:49 +0300

    fdskjdsofjo

--- src/lib/supabaseAdmin.ts
+++ src/lib/supabaseAdmin.ts
@@ -1,6 +1,10 @@
 // @/lib/supabaseAdmin.ts
 import { createClient } from "@supabase/supabase-js";
 
+if (typeof window !== "undefined") {
+  throw new Error("supabaseAdmin should only be used on the server side");
+}
+
 export const supabaseAdmin = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!,
